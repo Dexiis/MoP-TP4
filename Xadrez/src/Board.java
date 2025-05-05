023b42dc7df4427aa98cc7af193f3ef97b002ad8
Bernardo Aguiar <baguiar512@gmail.com>
2025-05-05 23:39:00 +0100

    Boar Interface + Functionality (Not finished)

--- Xadrez/src/Board.java
+++ Xadrez/src/Board.java
@@ -1,7 +1,8 @@
 import Pieces.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Board {
     private static Board instance = null;
@@ -41,7 +42,7 @@ public class Board {
         board[7][4] = PieceFactory.createPiece(Type.KING, Color.WHITE, false, false);
     }
 
-    public void checkForAvalibleMoves(Pieces piece) {
+    private HashSet<PiecePosition> checkForAvalibleMoves(Pieces piece) {
         int currentColPosition = 0;
         int currentRowPosition = 0;
         for (int row = 0; row < 8; row++) {
@@ -54,6 +55,115 @@ public class Board {
             }
         }
 
-        Set<PiecePosition> availableMoves = new HashSet<>(piece.checkForAvailableMoves(currentColPosition, currentRowPosition, board));
+        return new HashSet<>(piece.checkForAvailableMoves(currentColPosition, currentRowPosition, board));
+    }
+
+    public void printBoard() {
+        final int CELL_WIDTH = 5;
+
+        System.out.print("  ");
+        for (char colLabel = 'A'; colLabel <= 'H'; colLabel++) {
+            System.out.printf("%" + CELL_WIDTH + "s", " " + colLabel + " ");
+        }
+        System.out.println();
+
+        for (int row = 0; row < 8; row++) {
+            System.out.print("  ");
+            for (int col = 0; col < 8; col++) {
+                System.out.print("+----");
+            }
+            System.out.println("+");
+
+            int chessRowLabel = 8 - row;
+            System.out.print(chessRowLabel + " ");
+
+            for (int col = 0; col < 8; col++) {
+                Pieces currentPiece = board[row][col];
+
+                System.out.print("|");
+                String pieceRepresentation = "    ";
+
+                if (currentPiece != null) {
+                    String type = "";
+                    if (currentPiece instanceof Pawn) type = "P";
+                    else if (currentPiece instanceof Rook) type = "R";
+                    else if (currentPiece instanceof Knight) type = "K";
+                    else if (currentPiece instanceof Bishop) type = "B";
+                    else if (currentPiece instanceof Queen) type = "Q";
+                    else if (currentPiece instanceof King) type = "G";
+
+                    String color = (currentPiece.getColor() == Color.BLACK) ? "b" : "w";
+                    pieceRepresentation = " " + type + color + " ";
+                }
+
+                System.out.print(pieceRepresentation);
+
+            }
+            System.out.println("|");
+
+        }
+
+        System.out.print("  ");
+        for (int col = 0; col < 8; col++) {
+            System.out.print("+----");
+        }
+        System.out.println("+");
+
+        for (char colLabel = 'A'; colLabel <= 'H'; colLabel++) {
+            System.out.printf("%" + CELL_WIDTH + "s", "" + colLabel + "");
+        }
+        System.out.println();
+    }
+
+    public void movePiece(String piecePosition, String targetedPosition) {
+
+        if (piecePosition.length() != 2 || targetedPosition.length() != 2) {
+            System.out.println("Uma das posições é inválida!");
+            return;
+        } else {
+            int colC = 0, colT = 0;
+            int rowC, rowT;
+            char collC = piecePosition.charAt(0);
+            char rowwC = piecePosition.charAt(1);
+            char collT = targetedPosition.charAt(0);
+            char rowwT = targetedPosition.charAt(1);
+            Character[] colArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+            Character[] rowArray = {'1', '2', '3', '4', '5', '6', '7', '8'};
+            ArrayList<Character> colList = new ArrayList<>(Arrays.asList(colArray));
+            ArrayList<Character> rowList = new ArrayList<>(Arrays.asList(rowArray));
+
+            if (colList.contains(collC) && rowList.contains(rowwC) && colList.contains(collT) && rowList.contains(rowwT)) {
+
+                rowC = 8 - Integer.parseInt(String.valueOf(rowwC));
+                switch (collC) {
+                    case 'a' -> colC = 0;
+                    case 'b' -> colC = 1;
+                    case 'c' -> colC = 2;
+                    case 'd' -> colC = 3;
+                    case 'e' -> colC = 4;
+                    case 'f' -> colC = 5;
+                    case 'g' -> colC = 6;
+                    case 'h' -> colC = 7;
+                }
+                rowT = Integer.parseInt(String.valueOf(rowwT)) - 1;
+                switch (collT) {
+                    case 'a' -> colT = 0;
+                    case 'b' -> colT = 1;
+                    case 'c' -> colT = 2;
+                    case 'd' -> colT = 3;
+                    case 'e' -> colT = 4;
+                    case 'f' -> colT = 5;
+                    case 'g' -> colT = 6;
+                    case 'h' -> colT = 7;
+                }
+                PiecePosition PP = new PiecePosition(rowT, colT);
+                HashSet<PiecePosition> availableMoves = checkForAvalibleMoves(board[rowC][colC]);
+
+                if (availableMoves.contains(PP)) {
+                    board[rowT][colT] = board[rowC][colC];
+                    board[rowC][colC] = null;
+                } else System.out.println("Jogada Impossível");
+            }
+        }
     }
 }
--- Xadrez/src/ConsoleInterface.java
+++ Xadrez/src/ConsoleInterface.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class ConsoleInterface {
     private static ConsoleInterface instance = null;
     private Board board = Board.create();
+    private boolean finished = false;
+    private String piece;
+    private String targetedPosition;
+    Scanner input = new Scanner(System.in);
 
     private ConsoleInterface() {
     }
@@ -13,5 +19,14 @@ public class ConsoleInterface {
     }
 
     public void run() {
+        board.createBoard();
+        while (!finished) {
+            board.printBoard();
+            System.out.println("Escolha a posição da peça que deseja mover: ");
+            piece = input.nextLine();
+            System.out.println("Escolha o lugar para onde a deseja mover: ");
+            targetedPosition = input.nextLine();
+            board.movePiece(piece,targetedPosition);
+        }
     }
 }
